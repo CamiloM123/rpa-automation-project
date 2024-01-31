71b09d0cbcd3b56b334be683c7a6b52ce470df36
CamiloM <juanc.munozv@autonoma.edu.co>
2024-01-31 17:47:39 -0500

    Document and organize code.

--- colors.py
+++ colors.py
@@ -1,46 +1,31 @@
-def clasificar_color(color):
+def clasificar_color(color: any) -> str:
+    """
+    Clasifica un color dado en una de las categorías predefinidas.
+
+    Parameters:
+    - color (tuple): Una tupla que representa el color en formato RGB.
+
+    Returns:
+    - str: La categoría a la que pertenece el color ('morado', 'verde', 'rojo', 'azul', 'magenta', 'negro' o 'No clasificado').
+
+    Example:
+    >>> color_ejemplo = (127, 0, 255)
+    >>> categoria = clasificar_color(color_ejemplo)
+    >>> print(f"El color se clasifica como: {categoria}")
+    El color se clasifica como: morado
+    """
     R, G, B = color
 
-    # # Tonos de Morado
-    morado_1 = (127, 0, 255)
-    morado_2 = (127, 102, 204)
-    morado_3 = (191, 127, 255)
-    morado_4 = (76, 0, 153)
-    morado_5 = (153, 0, 153) 
-
-    # Tonos de Verde
-    verde_1 = (0, 255, 0)
-    verde_2 = (51, 204, 0)
-    verde_3 = (38, 153, 0)
-    verde_4 = (0, 127, 0)
-    verde_5 = (38, 76, 38)
-
-    # Tonos de Rojo
-    rojo_1 = (255, 0, 0)
-    rojo_2 = (204, 0, 0)
-    rojo_3 = (153, 0, 0)
-    rojo_4 = (204, 51, 0)
-    rojo_5 = (255, 127, 127)
-
-    # Color Azul
-    azul = (0, 0, 255)
-
-    # Color Magenta
-    magenta = (255, 0, 255)
-
-    # Color Negro
-    negro = (0, 0, 0)
-    # Funciones de comparación
-    
+    # Definir un diccionario con los tonos de colores
     diccionario = {
-        'morado' : {morado_1, morado_2, morado_3, morado_4, morado_5},
-        'verde' : {verde_1, verde_2, verde_3, verde_4, verde_5},
-        'rojo' : {rojo_1, rojo_2, rojo_3, rojo_4, rojo_5},
-        'azul' : {azul},
-        'magenta' : {magenta},
-        'negro' : {negro}
+        'morado': {(127, 0, 255), (127, 102, 204), (191, 127, 255), (76, 0, 153), (153, 0, 153)},
+        'verde': {(0, 255, 0), (51, 204, 0), (38, 153, 0), (0, 127, 0), (38, 76, 38)},
+        'rojo': {(255, 0, 0), (204, 0, 0), (153, 0, 0), (204, 51, 0), (255, 127, 127)},
+        'azul': {(0, 0, 255)},
+        'magenta': {(255, 0, 255)},
+        'negro': {(0, 0, 0)}
     }
-    
+
     # Buscar el color en el diccionario
     for categoria, colores in diccionario.items():
         if color in colores:
@@ -53,3 +38,4 @@ def clasificar_color(color):
 color_ejemplo = (127, 0, 255)
 categoria = clasificar_color(color_ejemplo)
 print(f"El color se clasifica como: {categoria}")
+
--- main.py
+++ main.py
@@ -1,94 +1,67 @@
 import pyautogui
 import time
+import os
 from colors import clasificar_color
 
-""" Mouse functions """
-
-time.sleep(3)
-# *** Information about the mouse ***
-print(pyautogui.position())  # (x, y) coordinates of the mouse
-# print(pyautogui.size()) # (width, height) of the screen
-# print(pyautogui.onScreen(-1, 0)) # True if (x, y) is within the screen.
-
-# *** Move the mouse ***
-# pyautogui.moveTo(1000, 100, duration=3)  # move mouse to (x, y) coordinates
-# pyautogui.moveRel(100, 200, duration=3)  # move mouse relative to its current position
-
-# *** Click the mouse ***
-# pyautogui.click(1000, 300, 4, 1, button="left")  # click the mouse at (x, y) coordinates
-# pyautogui.doubleClick(1000, 300)
-
-# *** Drag the mouse ***
-# pyautogui.dragTo(1000, 500, duration=3)  # drag mouse to XY
-# pyautogui.dragRel(100, 100, duration=3)  # drag mouse relative to its current position
-
-# # *** Scroll the mouse ***
-# pyautogui.scroll(100)  # scroll up 100 "clicks"
-# pyautogui.scroll(-100)  # scroll down 100 "clicks"
-
-""" Spiral drawing using pyautogui """
-
-# time.sleep(1)
-# distance = 300
-# while distance > 0:
-#     pyautogui.dragRel(distance, 0, 0.2, button="left")  # move right
-#     distance = distance - 20
-#     pyautogui.dragRel(0, distance, 0.2, button="left")  # move down
-#     pyautogui.dragRel(-distance, 0, 0.2, button="left")  # move left
-#     distance = distance - 20
-#     pyautogui.dragRel(0, -distance, 0.2, button="left")  # move up
-
-# time.sleep(2)
-
-""" Keyboard functions """
-
-# *** Information about the keyboard ***
-# print(pyautogui.KEYBOARD_KEYS)  # list of all keyboard keys
-# print(pyautogui.KEYBOARD_KEYS[0])  # first keyboard key
-
-# *** Press and write***
-# pyautogui.press("enter")  # press and release the enter key
-# pyautogui.write("Hello world!", interval=0.2)  # write "Hello world!" with a quarter second delay after each character
-# pyautogui.screenshot("screenshot.png")  # take a screenshot
-
-
-""" Image """
-
-import os
-
-image = os.path.join(os.path.dirname(__file__), 'origin.png')
-
-position = pyautogui.locateOnScreen(image)
-
-if position is not None:
-    # Imprime las coordenadas de la esquina superior izquierda y la esquina inferior derecha
-    print(f"La imagen fue encontrada en la posición: {position}")
-else:
-    print("La imagen no fue encontrada en la pantalla.")
-
-""" Colors """
-
 def get_color(x, y):
+    """
+    Obtiene el color de la pantalla en las coordenadas dadas.
+
+    Parameters:
+    - x (int): Coordenada x.
+    - y (int): Coordenada y.
+
+    Returns:
+    - tuple: Una tupla que representa el color en formato RGB.
+    """
     return pyautogui.pixel(x, y)
 
-pointer_x, pointer_y = pyautogui.center(position)
-pointer_y += 40
-i = 1
+def find_image(image_path):
+    """
+    Busca una imagen en la pantalla y devuelve su posición si es encontrada.
 
-while True:
-    color = get_color(pointer_x, pointer_y)
-    if color == (255, 255, 255):
-        print("Color blanco encontrado.")
-        break
+    Parameters:
+    - image_path (str): La ruta de la imagen a buscar.
+
+    Returns:
+    - tuple or None: La posición de la esquina superior izquierda y la esquina inferior derecha de la imagen si es encontrada,
+    o None si no se encuentra.
+    """
+    position = pyautogui.locateOnScreen(image_path)
+    return position
+
+def main():
+    time.sleep(3)
+    print(pyautogui.position())  # (x, y) coordinates of the mouse
+
+    image_path = os.path.join(os.path.dirname(__file__), 'origin.png')
+    position = find_image(image_path)
+
+    if position is not None:
+        # Imprime las coordenadas de la esquina superior izquierda y la esquina inferior derecha
+        print(f"La imagen fue encontrada en la posición: {position}")
     else:
-        color_name = clasificar_color(color)
-        print(f"color {i}: {color} -> {color_name}")
-        pyautogui.moveTo(pointer_x, pointer_y)  # move mouse to (x, y) coordinates
-        i += 1
-        if i != 10:
-            pointer_y += 20
-        else:
-            pointer_y += 15
-    time.sleep(0.5)
+        print("La imagen no fue encontrada en la pantalla.")
 
-#Error 10-11
+    pointer_x, pointer_y = pyautogui.center(position)
+    pointer_y += 40
+    i = 1
+
+    while True:
+        color = get_color(pointer_x, pointer_y)
+        if color == (255, 255, 255):
+            print("Color blanco encontrado.")
+            break
+        else:
+            color_name = clasificar_color(color)
+            print(f"color {i}: {color} -> {color_name}")
+            pyautogui.moveTo(pointer_x, pointer_y)  # mueve el mouse a las coordenadas (x, y)
+            i += 1
+            if i != 10:
+                pointer_y += 20
+            else:
+                pointer_y += 15
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    main()
