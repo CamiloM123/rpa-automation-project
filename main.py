3d9598fc213117a41493d31feb84f276f4a5d8f0
CamiloM <juanc.munozv@autonoma.edu.co>
2024-02-12 15:46:51 -0500

    Create and modified the executable.

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ Author: Juan Camilo Muñoz Valencia
 This module contains the main function to execute the automation script.
 """
 
-import pyautogui
+import pyautogui 
 import time
-from functions import get_color, color_process, move_to_center, color_params, delete_and_write
-
+from functions import get_color, color_process, move_to_center, color_params, delete_and_write, resourse_path
 
 def black_priority():
     """
     Move the mouse to the center of the black priority image and adjust position.
     """
-    pointer_x, pointer_y = move_to_center('images/priority.png')
-    pointer_x += 100
+    path = resourse_path('priority.PNG')
+    pointer_x, pointer_y = move_to_center(path)
+    pointer_x += 80
     pyautogui.moveTo(pointer_x, pointer_y)
+    delete_and_write('99')
 
 
 def change_velocity():
@@ -23,7 +24,8 @@ def change_velocity():
     Change the velocity parameter.
     """
     time.sleep(0.5)
-    pointer_x, pointer_y = move_to_center("images/velocity-change.png")
+    path = resourse_path('velocity-change.PNG')
+    pointer_x, pointer_y = move_to_center(path)
     pointer_x += 100
     pyautogui.moveTo(pointer_x, pointer_y)
 
@@ -33,12 +35,14 @@ def change_power():
     Change the power parameter.
     """
     time.sleep(0.5)
-    pointer_x, pointer_y = move_to_center("images/min-power-change.png")
+    path_1 = resourse_path('min-power-change.PNG')
+    pointer_x, pointer_y = move_to_center(path_1)
     pointer_y += 30
     pyautogui.moveTo(pointer_x, pointer_y)
 
+    path_2 = resourse_path('max-power-change.PNG')
     time.sleep(0.5)
-    pointer_x2, pointer_y2 = move_to_center("images/max-power-change.png")
+    pointer_x2, pointer_y2 = move_to_center(path_2)
     pointer_y2 += 30
     pyautogui.moveTo(pointer_x2, pointer_y2)
 
@@ -48,7 +52,8 @@ def change_interval():
     Change the interval parameter.
     """
     time.sleep(0.5)
-    pointer_x, pointer_y = move_to_center("images/interval-change.png")
+    path = resourse_path('interval-change.PNG')
+    pointer_x, pointer_y = move_to_center(path)
     pointer_x += 100
     pyautogui.moveTo(pointer_x, pointer_y)
 
@@ -72,13 +77,15 @@ def black_priority_process():
     Process black priority.
     """
     time.sleep(3)
-    pointer_x, pointer_y = move_to_center("images/origin-1.png")
-    pointer_y += 8
+    path = resourse_path('origin-1.PNG')
+    pointer_x, pointer_y = move_to_center(path)
+    pointer_y += 6
     pyautogui.moveTo(pointer_x, pointer_y)
 
     while True:
         color = get_color(pointer_x, pointer_y)
         if color == (0, 0, 0):
+            pyautogui.click(pointer_x, pointer_y)
             print("Black color found.")
             black_priority()
             pyautogui.moveTo(pointer_x, pointer_y)
@@ -86,6 +93,7 @@ def black_priority_process():
         else:
             pointer_y += 15
             pyautogui.moveTo(pointer_x, pointer_y)
+            pyautogui.click(pointer_x, pointer_y)
             time.sleep(1)
 
 
@@ -93,8 +101,9 @@ def param_library_process(color_name):
     """
     Process the parameters library.
     """
-    pointer_x, pointer_y = move_to_center('images/param-library.png')
-    pyautogui.moveTo(pointer_x, pointer_y)
+    path = resourse_path('param-library.PNG')
+    pointer_x, pointer_y = move_to_center(path)
+    pyautogui.click(pointer_x, pointer_y)
     color_params(color_name)
     # pyautogui.click()
 
@@ -104,7 +113,12 @@ def procces_2():
     Process the second stage.
     """
     # time.sleep(3)
-    pointer_x, pointer_y = move_to_center('images/origin-2.png')
+    # path = resourse_path('images/origin-2.PNG')
+    path = resourse_path('origin-2.PNG')
+    pointer_x, pointer_y = move_to_center(path)
+    if pointer_x == 0 and pointer_y == 0:
+        print("Reload the program with the image in the screen.")
+        return
     pointer_y += 23
     i = 1
     while True:
@@ -114,20 +128,24 @@ def procces_2():
             break
         else:
             color_name = color_process(color, pointer_x, pointer_y, i)
+            pyautogui.click(pointer_x, pointer_y)
             time.sleep(0.5)
             # param_library_process(color_name)
             # time.sleep(0.5)
-            params_change_process(color_name)
+            # params_change_process(color_name)
             time.sleep(0.5)
             if color_name in ["green", "purple", "red"]:
-                x, y = move_to_center('images/check-1.png')
-                x -= 60
-                pyautogui.moveTo(x, y)
+                path = resourse_path('check-1.PNG')
+                x, y = move_to_center(path)
+                if x != 0 and y != 0:
+                    x -= 60
+                    pyautogui.moveTo(x, y) 
+                    pyautogui.click(x, y)
 
             if color_name != "green":
-                pointer_y += 15
+                pointer_y += 13
             else:
-                pointer_y += 20
+                pointer_y += 17
             i += 1
         time.sleep(1)
 
@@ -136,10 +154,12 @@ def procces_3():
     """
     Process the third stage.
     """
-    pointer_x, pointer_y = move_to_center('images/btn-ok.png')
+    path = resourse_path('btn-ok.PNG')
+    pointer_x, pointer_y = move_to_center(path)
     pyautogui.click(pointer_x, pointer_y)
     time.sleep(3)
-    pointer_x, pointer_y = move_to_center('images/btn-download.png')
+    path = resourse_path('btn-download.PNG')
+    pointer_x, pointer_y = move_to_center(path)
     pyautogui.click(pointer_x, pointer_y)
 
 
@@ -148,10 +168,9 @@ def main():
     Main function to execute the automation script.
     """
     time.sleep(3)
-    # black_priority_process()
-    procces_2()
+    black_priority_process()
+    # procces_2()
     # procces_3()
-
 
 if __name__ == "__main__":
     main()
